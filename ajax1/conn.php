8dd006fff48abe87618cccd8e61498f6b93c8a63
Michal-Sivicek <73881006+Michal-Sivicek@users.noreply.github.com>
2022-11-04 14:32:00 +0100

    update
    update

---
+++ ajax1/conn.php
@@ -0,0 +1,8 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "student";
+$database = "coffe_lmsoft_cz";
+
+$conn = new mysqli($host, $user, $password, $database);
+?>
--- ajax1/index.php
+++ ajax1/index.php
@@ -1,4 +1,29 @@
 <?php
+session_start();
+
+include 'conn.php';
+global $conn;
+
+error_reporting(0);
+
+if (isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+if (isset($_POST['submit'])) {
+	$email = $_POST['email'];
+	$password = md5($_POST['password']);
+
+	$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+	$result = mysqli_query($conn, $sql);
+	if ($result->num_rows > 0) {
+		$row = mysqli_fetch_assoc($result);
+		$_SESSION['username'] = $row['username'];
+		header("Location: index.php");
+	} else {
+		echo "<script>alert('Woops! Email or Password is Wrong.')</script>";
+	}
+}
 
 ?>
 <!DOCTYPE html>
@@ -7,7 +32,7 @@
         <meta charset="utf-8">
         <title>Uloha AJAX1</title>
         <script src="./script.js"></script>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="styles.css">
     </head>
     <body>
         <main>
